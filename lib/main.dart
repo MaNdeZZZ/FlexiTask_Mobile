cfd00a0ef7b012bb7ebbd351550f4bdca3d53699
Ray <raihanarsy2004@gmail.com>
2025-03-31 01:12:23 +0700

    Update fitur Firebase integrasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flexitask_mobile/loading_page.dart';
 import 'package:flutter/material.dart';
 
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized(); // Pastikan binding diinisialisasi sebelum Firebase
+//   await Firebase.initializeApp(); // Inisialisasi Firebase
+//   runApp(MyApp());
+// }
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+    print("✅ Firebase berhasil diinisialisasi!");
+  } catch (e) {
+    print("❌ Firebase gagal diinisialisasi: $e");
+  }
   runApp(MyApp());
 }
 
